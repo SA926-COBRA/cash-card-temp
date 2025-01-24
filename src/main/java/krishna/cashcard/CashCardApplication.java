f601fedd66dbcd047e037a4ab25f3cccd0d8a7c5
SA926-COBRA <sayanacar926@gmail.com>
2025-01-25 00:52:14 +0545

    chore: update package name from example to krishna

--- src/main/java/krishna/cashcard/CashCardApplication.java
+++ src/main/java/krishna/cashcard/CashCardApplication.java
@@ -1,4 +1,4 @@
-package example.cashcard;
+package krishna.cashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/krishna/cashcard/CashCardApplicationTests.java
+++ src/test/java/krishna/cashcard/CashCardApplicationTests.java
@@ -1,4 +1,4 @@
-package example.cashcard;
+package krishna.cashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
