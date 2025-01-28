bddf2d92eef8883ecc509de04b97f55d6d2fa743
SA926-COBRA <sayanacar926@gmail.com>
2025-01-28 22:48:12 +0545

    use the CashCardRepository for the Data Management

--- src/main/java/krishna/cashcard/CashCardController.java
+++ src/main/java/krishna/cashcard/CashCardController.java
@@ -1,6 +1,7 @@
 package krishna.cashcard;
 
 import org.springframework.http.ResponseEntity;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
-    private final CashCardRepository cashCardRepository;
 
-    private CashCardController(CashCardRepository cashCardRepository){
-        this.cashCardRepository = cashCardRepository;
+    private final CashCardRepository CashCardRepository;
+
+    private CashCardController(CashCardRepository CashCardRepository){
+        this.CashCardRepository = CashCardRepository;
     }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = CashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
